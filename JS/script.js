2177a3b8530e32c5a01916a87fbf082502a22f7f
AleAlle62 <basketallegrini@gmail.com>
2022-10-22 18:00:10 +0200

    0.04

--- JS/script.js
+++ JS/script.js
@@ -1,69 +1,81 @@
 /*
-
-ESERCIZIO 
-
-Scrivere un programma che chieda all’utente:
-- Il numero di chilometri da percorrere
-- Età del passeggero
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
-- il prezzo del biglietto è definito in base ai km (0.21 € al km)
-- va applicato uno sconto del 20% per i minorenni
-- va applicato uno sconto del 40% per gli over 65.
-
-svolgimento:
-
-1) fare il primo box
-2) fare i button che funzionino con i diversi input
-3) fare il bottone annulla
-4) dare tutti i risultati nel box di sotto 
-5) colegare il bottone genera come invio dei dati 
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
 
+// ideazione del programma 
 
+/*
+
+1) chiedere all'utente num km e eta passeggero
+2) OBBIETTIVO : calcolare il costo del viaggio
+3) COME CALCOLARE IL COSTO: 
+    . 0,21 a km
+    . - 20% ai < 18anni
+    . - 40% a > 65anni
+
+4) dare il risultato in output con due cifre dopo la virgola.
+
+*/
 
 // costanti 
 
-// let kilometri =  ('quanti km vuoi fare ?');
-// let age =  ('quanti anni hai ?');
-
-
-let name = document.getElementById('#namelastname')
-let kilometres = document.getElementById('#km')
-let years = document.getElementById('#age')
+// let kilometri = document.getElementById('km')
+let age = document.getElementById('eta')
+// let cost = (kilometri * 0.21).toFixed(2);
 let btnCliccami = document.querySelector('.bottone') 
 
 
 
-// .VALUE E PARSEINT UN PO OVUNQUE METTERE !!!!!!!!!!!!!!!!!!!!!!!!
+
+const pricePerKilometer = 0.21;
+let basePrice = pricePerKilometer * 0.21;
+let discountRate = 0;
 
 
-// creare l'evento per cui se clicchi il bottone genera il biglietto 
+// formula sconti 
+
 
 
 btnCliccami.addEventListener ('click', function () {
+    // if (age < 18) {
+    //     let sale = (cost * 0.8).toFixed(2);
+    //     console.log(sale + '€');
+    // } 
     
-    let kilometres = parseFloat(km.value)
-    let prezzobase = parseFloat(km * 0.21)
+    // else if (age > 65) {
+    //     let sale = (cost * 0.6).toFixed(2);
+    //     console.log(sale + '€');
+    // } else {
+    //     console.log (cost + '€' );
+    // }
 
-
-    if (age <= 18) {
-        let sale = (prezzobase * 0.8).toFixed(2);
-        console.log(sale + '€');
-    } 
-
-    else if (age >= 65) {
-        let sale = (prezzobase * 0.6).toFixed(2);
-        console.log(sale + '€');
-    } else {
-        console.log (prezzobase + '€' );
+    if (age < 18) {
+        discountRate = totalPrice * 0.4;
+    } else if (age >= 65) {
+        discountRate = totalPrice  * 0.6;
     }
-
+    
+    // alternativa A
+    let totalPrice = basePrice - basePrice * discountRate / 100;
+    totalPrice = parseFloat(totalPrice.toFixed(2));
+    console.log('Il prezzo del biglietto è: ' + totalPrice);
 })
 
 
-// evento per cui si annulla tutto del biglietto 
 
-// let btnAnnulla = document.querySelector('.bottoneannulla'); 
+
+// collegamenti 
+
+// window.document.getElementById('km').innerHTML = kilometri + 'km';
+// window.document.getElementById('eta').innerHTML = age + 'y.o.';
+// window.document.getElementById('sconto').innerHTML = cost + '€';
+
+
 
 
 
